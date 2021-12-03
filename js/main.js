bb6eb68e939017f475b644df6e3055dcf8c5f301
Paloma Lozano <palomalogu9@gmail.com>
2021-12-03 12:46:01 +0100

    created two functions for less code

--- js/main.js
+++ js/main.js
@@ -2,16 +2,25 @@
 
 const cardsElement = document.querySelector('.js-cards');
 
-function getProductCode() {
+function getProductHtmlCode(name, price, imgUrl) {
   let htmlCode = `<article class="card">`;
-  htmlCode += `<img src="./images/node-js.jpg" class="card__img" alt="Camiseta de Node JS">`;
-  htmlCode += `<h3 class="card__title">Node JS</h3>`;
-  htmlCode += `<p class="card__description">12,00 €</p>`;
+  htmlCode += `<img src="${imgUrl}" class="card__img" alt="Camiseta de ${name}">`;
+  htmlCode += `<h3 class="card__title">${name}</h3>`;
+  htmlCode += `<p class="card__description">${price}€</p>`;
   htmlCode += `<button class="card__btn">Añadir a la cesta</button>`;
   htmlCode += `</article>`;
   return htmlCode;
 }
 
-const htmlCode = getProductCode();
+function paintPorducts() {
+  const product1 = getProductHtmlCode('Node JS', 12.0, './images/node-js.jpg');
+  const product2 = getProductHtmlCode(
+    'JavaScript',
+    15.0,
+    './images/javascript.jpg'
+  );
+  const product3 = getProductHtmlCode('React', 13.0, './images/react.jpg');
+  cardsElement.innerHTML = product1 + product2 + product3;
+}
 
-cardsElement.innerHTML = htmlCode;
+paintPorducts();
